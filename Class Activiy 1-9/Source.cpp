b61ef6e311b99bceb730f020c4da4446ea9508df
krojas2020 <54904828+krojas2020@users.noreply.github.com>
2020-01-09 10:38:18 -0700

    finshed part one

--- Class Activiy 1-9/Source.cpp
+++ Class Activiy 1-9/Source.cpp
@@ -3,9 +3,20 @@
 // jan. 9 
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+
+	string str;
+		cout << "Enter a Value" << endl;
+		cout << "One pound is 0.454 kg" << endl; 
+		double lbs;
+		double kgs;
+		kgs = 0.454 * lbs;
+		lbs = 2.2 * kgs;
+	
+	return 0;
 
 }
